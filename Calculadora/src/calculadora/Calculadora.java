db2aa0d3a5c6800fba424814e533396d991d076d
Daniel <revolverocelot.r3v0lv3r0c3l0t@gmail.com>
2019-04-25 21:01:13 -0300

    Upando as basic files

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    
+    public static void main(String[] args) {
+        String strteste[] = new String[3];
+        Scanner scanf = new Scanner(System.in);
+        String operacao;
+        do{
+            System.out.println("Insira os valores e o operador:"); 
+            operacao = scanf.nextLine();
+            strteste = operacao.split(" ");
+            double x = Double.parseDouble(strteste[0]);
+            double y = Double.parseDouble(strteste[2]);
+            switch(strteste[1]){
+                case "+":
+                    System.out.println(Soma.calcSoma(x, y));
+                    break;
+                case "-":
+                    System.out.println(Subtracao.calcSub(x, y));
+                    break;
+                case "*":
+                    System.out.println(Multiplicacao.calcMult(x, y));
+                    break;
+                case "/":
+                    System.out.println(Divisao.calcDiv(x, y));
+                    break;
+                default:
+                    System.out.println("Insira uma operação válida");
+            }       
+        }while(! operacao.equals("exit"));
+    }
+}
